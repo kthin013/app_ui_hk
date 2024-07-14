3d262669fc4357a6d290ec51f5d592ee63facf62
kthin013 <tamyuhin130101@gmail.com>
2024-07-15 01:03:29 +0800

    first commit

---
+++ components/StyledButton.vue
@@ -0,0 +1,50 @@
+<template>
+	<button :style="computedStyles">
+		<slot></slot>
+	</button>
+</template>
+
+<script>
+	export default {
+		name: 'StyledButton',
+		props: {
+			width: {
+				type: String,
+				default: 'auto'
+			},
+			height: {
+				type: String,
+				default: 'auto'
+			},
+			borderRadius: {
+				type: String,
+				default: '24rpx'
+			},
+			backgroundColor: {
+				type: String,
+				default: '#3498db'
+			}
+		},
+		computed: {
+			computedStyles() {
+				return {
+					width: this.width,
+					height: this.height,
+					borderRadius: this.borderRadius,
+					backgroundColor: this.backgroundColor,
+					color: 'white', // Default text color
+					padding: '14rpx, 24rpx, 14rpx, 24rpx',
+					border: 'none',
+					cursor: 'pointer',
+					transition: 'background-color 0.3s'
+				};
+			}
+		}
+	};
+</script>
+
+<style scoped>
+	button:hover {
+		background-color: darken(#3498db, 10%);
+	}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,54 @@
+<template>
+	<view class="main-container">
+		<view class="column">
+			<view class="title-container">
+				<text class="title-text-24">Login/Sign</text>
+			</view>
+			<wd-input type="text" v-model="value" style="margin-bottom: 33rpx;" />
+			<StyledButton width="100%" height="content-fit" backgroundColor="#161616" borderRadius="24rpx">
+				<text class="title-text-24" style="font-size: 20rpx;">Next</text>
+			</StyledButton>
+		</view>
+	</view>
+</template>
+
+<script>
+	import StyledButton from "../../components/StyledButton.vue"
+
+	export default {
+		data() {
+			return {
+				title: 'Hellod'
+			}
+		},
+		onLoad() {
+
+		},
+		components: {
+			StyledButton
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.column {
+		display: flex;
+		flex-direction: column;
+		background-color: yellow;
+		border: black 3rpx solid;
+	}
+
+	.title-container {
+		margin: 25rpx 8rpx 56rpx 8rpx
+	}
+
+	.button-text {
+		font-family: Manrope;
+		font-size: 24rpx;
+		font-weight: 800;
+		line-height: 24rpx;
+	}
+</style>
